740e3d4083ca452335faa4a1fb24bc979bfc5323
mariehposa <mariamadedeji.work@gmail.com>
2023-05-09 23:46:38 +0100

    attempt fix CORS issue

--- src/socket.js
+++ src/socket.js
@@ -3,4 +3,6 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? "https://suitability.up.railway.app" : 'http://localhost:4000';
 
-export const socket = io(URL);
+export const socket = io(URL, {
+    withCredentials: true,
+});
