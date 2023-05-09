9437368a16810af4bc46a1d78d94314ab68b2cfc
mariehposa <mariamadedeji.work@gmail.com>
2023-05-10 00:12:24 +0100

    attempt fix CORS issue

--- src/socket.js
+++ src/socket.js
@@ -4,5 +4,5 @@ import { io } from 'socket.io-client';
 const URL = process.env.NODE_ENV === 'production' ? "https://suitability.up.railway.app" : 'http://localhost:4000';
 
 export const socket = io(URL, {
-    withCredentials: true,
+    // withCredentials: true,
 });
