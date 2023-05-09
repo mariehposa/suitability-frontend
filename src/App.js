d659c42d57c8cd38b4dca55ab19f8a12c97516da
mariehposa <mariamadedeji.work@gmail.com>
2023-05-09 20:41:23 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Board from "./components/Board";
+import Notification from "./components/Notification";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	
+	return (
+		<div className="App">
+			<Board />
+      <Notification />
+		</div>
+	);
 }
 
 export default App;
