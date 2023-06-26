fff1d33f0623eff0cb198d1432bc1fc3a59cd1ab
Maaruf Dauda <mkdauda.work@gmail.com>
2023-06-26 10:26:03 +0100

    update app constants

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,6 +9,9 @@ export const socketConstants = {
 	playerTricks: "playerTricks",
 	chooseSuitability: "chooseSuitability",
 	suitabilityChosen: "suitabilityChosen",
+	createRoom: 'createRoom',
+	joinRoom: 'joinRoom',
+	connectedPlayers: 'connectedPlayers',
 
 	
 	connect: "connect",
@@ -20,4 +23,19 @@ export const gameState = {
 	chooseMode: "choose_mode",
 	inPlay: "in_play",
 	selectSuitabilities: "select_suitabilities"
+}
+
+export const privileges = {
+	Immunity: 'immunity',
+	Warranty: 'warranty',
+	Poverty: 'poverty',
+	None: 'none'
+
+}
+
+export const abilities = {
+	Trump: "Trump",
+	Reverse: "Reverse",
+	Penalty: "Penalty",
+	// Privilege: "Privilege"
 }
