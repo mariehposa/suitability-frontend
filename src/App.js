1e3a924ca69b506a9bb70b95fafe7da55bdd5091
mariehposa <mariamadedeji.work@gmail.com>
2023-07-03 16:38:43 +0100

    render rules

--- src/App.js
+++ src/App.js
@@ -4,39 +4,44 @@ import { PersistGate } from "redux-persist/lib/integration/react";
 import { persistor, store } from "./store";
 import Board from "./components/Board";
 import Notification from "./components/Notification";
+import Rules from "./components/Rules";
 import { socket } from "./socket";
 import { socketConstants } from "./utils/index.js";
 import { setUserId } from "./redux/actionCreators/user";
 import "./App.css";
 
 function App() {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const user = useSelector((state) => state.user);
-  useEffect(() => {
-    // no-op if the socket is already connected
-    socket.connect();
+	const user = useSelector((state) => state.user);
+	useEffect(() => {
+		// no-op if the socket is already connected
+		socket.connect();
 
-    socket.on(socketConstants.connect, () => {
-      dispatch(setUserId(socket.id));
-    });
+		socket.on(socketConstants.connect, () => {
+			dispatch(setUserId(socket.id));
+		});
 
-    return () => {
-      socket.off(socketConstants.connect);
-      socket.disconnect();
-    };
-  }, []);
+		return () => {
+			socket.off(socketConstants.connect);
+			socket.disconnect();
+		};
+	}, []);
 
-  return (
-    <Provider store={store}>
-      {/* <PersistGate loading={<Board />} persistor={persistor}> */}
-        <div className="App">
-          <Board />
-          <Notification />
-        </div>
-      {/* </PersistGate> */}
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			{/* <PersistGate loading={<Board />} persistor={persistor}> */}
+			<div className="App">
+				<div className="field">
+					<p className="game-name">Suitability</p>
+					<Board />
+					<Rules />
+				</div>
+				<Notification />
+			</div>
+			{/* </PersistGate> */}
+		</Provider>
+	);
 }
 
 export default App;
